b21cb7a46c876fae9e58a87c63e4eba544078fd3
obedest <161562219+obedest@users.noreply.github.com>
2025-07-22 22:02:37 +1000

    Solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num >= 0) {
+        return 'Error';
+    }
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
 };
+
+// repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
