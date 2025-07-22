593707011da6dbc09f5d0f561307be6e709d80da
obedest <161562219+obedest@users.noreply.github.com>
2025-07-22 23:21:21 +1000

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, ...remove) {
+//     let indexArray = []
+//     for (let i = 0; i < arr.length; i++) {
+//         for (const item of remove) {
+//             if (arr[i] === item) {
+//                 indexArray.push(i);
+//             }
+//         }
+//     }
+//     indexArray.reverse();
+//     for ( const index of indexArray) {
+//         arr.splice(index, 1);
+//     }
+//     return arr;
+// };
+
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(item => !remove.includes(item));
 };
 
 // Do not edit below this line
