070495504cf3eb01186b189eba5dca0e5d344d7b
obedest <161562219+obedest@users.noreply.github.com>
2025-07-22 23:50:17 +1000

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 % 1 != 0 || num2 % 1 != 0 || num1 < 0 || num2 < 0) {
+        return "Error"
+    }
+    let sum = 0;
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
